2edfc8ecab2bcc9bb55854cc6366e9398c476b42
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-14 15:02:32 +0200

    Crezione della classe Address

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Address
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string Province { get; private set; }
+        public int Zip { get; private set; }
+
+        public Address(string name,string surname, string street, string city, string province, int zip)
+        {
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            Zip = zip;
+        }
+    }
+}
