c364556e19287e3c3e35bda55dcbbce420f7d811
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-14 16:39:23 +0200

    Aggiunto un try catch per la lettura del file

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -9,39 +9,44 @@ using System.IO.Enumeration;
 List<Address> ReadFile()
 {
     List<Address> addresses = new List<Address>();
-
-    StreamReader readedFile = File.OpenText("C:\\Users\\user\\Desktop\\DotNet\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\my-addresses.csv");
-
-    int i = 0;
-    while(!readedFile.EndOfStream)
+    try
     {
-        string line = readedFile.ReadLine().TrimStart(',');
-        
-        if(i > 0)
-        {
-            string[] splitedLine = line.Split(",");
-            
-            if(splitedLine.Length == 6)
-            {
-                string name = splitedLine[0];
-                string surname = splitedLine[1];
-                string street = splitedLine[2];
-                string city = splitedLine[3];
-                string province = splitedLine[4];
-                int zip = int.Parse(splitedLine[5]);
-                Address address = new Address(name, surname, street, city, province,zip);
-                addresses.Add(address);
-               
-            }else
-            {
-                Console.WriteLine($"L'indirizzo {line} non è corretto");
-            }
-        }
-        i++;
-    }
-   
-    readedFile.Close();
+        StreamReader readedFile = File.OpenText("C:\\Users\\user\\Desktop\\DotNet\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\my-addresses.csv");
 
+        int i = 0;
+        while(!readedFile.EndOfStream)
+        {
+            string line = readedFile.ReadLine().TrimStart(',');
+
+            if(i > 0)
+            {
+                string[] splitedLine = line.Split(",");
+
+                if(splitedLine.Length == 6)
+                {
+                    string name = splitedLine[0];
+                    string surname = splitedLine[1];
+                    string street = splitedLine[2];
+                    string city = splitedLine[3];
+                    string province = splitedLine[4];
+                    int zip = int.Parse(splitedLine[5]);
+                    Address address = new Address(name, surname, street, city, province, zip);
+                    addresses.Add(address);
+
+                }
+                else
+                {
+                    Console.WriteLine($"L'indirizzo {line} non è corretto");
+                }
+            }
+            i++;
+        }
+        readedFile.Close();
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
     return addresses;
 }
 
@@ -65,7 +70,7 @@ void PrintFile(string fileName, List<Address> addresses)
 
 // MAIN
 List<Address> addresses = ReadFile();
-foreach( Address address in addresses)
+foreach(Address address in addresses)
 {
     Console.WriteLine(address);
 }
